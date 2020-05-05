02e2f2534ef9a12ba216e39789149e2749e30666
José Mauro Neto <jmauroneto@gmail.com>
2020-05-05 16:11:17 -0300

    Pegando lista da Api e renderizando com o CardProduto

---
+++ src/components/CardProduto.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import IconButton from '@material-ui/core/IconButton';
+import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
+
+const ContainerProduto = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+`
+
+const Imagem = styled.img`
+    max-width: 100%;
+    max-height: 150px;
+`
+
+//componente que retorna cada produto individualmente, chamado na SecaoProdutos.js passando props
+class CardProduto extends React.Component {
+    render(){
+        const valor=this.props.valor, parcelas=this.props.parcelas
+        const valorParcela=valor/parcelas;
+        return <ContainerProduto>
+            <Imagem src={this.props.linkImagem} />
+            {this.props.pagamento==='card' 
+                ? <p>R${this.props.valor.toFixed(2)} em {this.props.parcelas}x de R${valorParcela.toFixed(2)} no cartão</p>
+                : <p>R${this.props.valor.toFixed(2)} à vista</p>
+            }
+            <IconButton onClick={() => this.props.passarCarrinho(this.props.id)}>
+                <AddShoppingCart />
+            </IconButton>
+            <h3>{this.props.nome}</h3>
+            <p>{this.props.descricao}</p>
+        </ContainerProduto>
+    }
+}
+
+export default CardProduto;
