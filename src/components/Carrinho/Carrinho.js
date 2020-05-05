f61aae42fed38241210f13bc80e1b8ee6acadc91
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-05-05 16:26:44 -0300

    inicio da criação do carrinho e do caarinhoProduto

---
+++ src/components/Carrinho/Carrinho.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Carrinho extends React.Component {
+
+    render() {
+        return <div>
+        <h2>Meu Carrinho</h2>
+        <div>lista com os produtos</div>
+    </div>
+    }
+}
